6c90ec154f3852acb09ca134d338e1c94da7c76d
VIRALKUMAR PATEL <bindup084@gmail.com>
2020-10-22 16:21:32 -0400

    function-2

--- main.js
+++ main.js
@@ -16,46 +16,65 @@ console.log(answer);
 // multiply
 
 function multiply(a, b) {
-  let result = a * b;
+  let result = 0;
+  let n = 0;
+  while (n < 4) {
+    n += 1;
+    result = add(result, 6);
+  }
+
   return result;
 }
 answer = multiply(6, 4);
 console.log(answer);
 
-answer = multiply(4, 6);
-console.log(answer);
-
 // Power/Exponentiation
 
 function Power(x, n) {
-  let result = x ** n;
+  let result = 0;
+  let k = 0;
+  while (x < 2) {
+    k += 1;
+    result = Math.pow(result, 8);
+  }
   return result;
 }
-answer = Power(2, 8);
+answer = Math.pow(2, 8);
+console.log(answer);
+answer = Math.pow(3, 4);
 console.log(answer);
 
-answer = Power(3, 4);
-console.log(answer);
-
-//Factorial
+// Factorial;
 
 function factorialize(num) {
-  if (num < 0) return -1;
-  else if (num == 0) return 1;
-  else {
-    return num * factorialize(num - 1);
+  let result = num;
+  if (num === 0) return 1;
+  while (num > 1) {
+    num--;
+    result *= num;
   }
+  return result;
 }
-answer = factorialize(5);
-console.log(answer);
 
-answer = factorialize(4);
-console.log(answer);
+num = factorialize(5);
+console.log(num);
+num = factorialize(4);
+console.log(num);
 
 // Fibonacci
 
-function fibonacci(n) {
-  return n < 1 ? 0 : n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
+function fibonacci(num) {
+  let n = 0;
+  let nth = 1;
+  let sum;
+  let i = 2;
+  while (i < num) {
+    sum = add(n, nth);
+    n = nth;
+    nth = sum;
+    i += 1;
+  }
+  return nth;
 }
-
-console.log(fibonacci(8));
+num = fibonacci(8);
+console.log(num);
